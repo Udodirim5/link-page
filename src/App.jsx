35bc046690f334002fb69ae76766cf09bb59e938
udodirim <udodirimwisdom@gmail.com>
2025-01-31 16:07:36 +0100

    update routing to support dynamic base path based on environment

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import process from "process";
+
+const isProduction = process.env.NODE_ENV === "production";
+const base = isProduction ? "/link-page" : "";
 
 const App = () => {
   return (
     <div className="app">
       <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+        <Route path={`${base}/`} element={<HomePage />} />
+        <Route path="*" element={<HomePage />} />
+        </Routes>
     </div>
   );
 };
