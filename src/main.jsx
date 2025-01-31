eaa9df46547bdc37cd3eed7105ce5d88106f2209
udodirim <udodirimwisdom@gmail.com>
2025-01-31 15:49:59 +0100

    set dynamic basename for BrowserRouter based on environment

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import process from 'process';
+
+const isProduction = process.env.NODE_ENV === "production";
+const basename = isProduction ? "/link-page" : "/";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
+    <BrowserRouter basename={basename}>
+    <App />
     </BrowserRouter>
   </StrictMode>
 );
